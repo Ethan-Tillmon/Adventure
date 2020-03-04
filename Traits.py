6ad3cd85c912bb37806db9f88a7d7bee05365ad4
Ethan-Tillmon <ethantillmon@amdg.rockhursths.edu>
2020-03-04 08:50:54 -0600

    Orphan traits

--- Traits.py
+++ Traits.py
@@ -1,3 +1,10 @@
 class traits(objects):
     race = ["black", "caucasian", "hispanic", "asian", "middle eastern"]
     height = ["6'03", "6'02", "6", "5'11", "5'10", "5'09", "5'08"]
+    skincolor = ["black", "light black", "brown", "light brown", "tan", "white"]
+    haircolor = ["brunette", "blond", "dirty blond", "red"]
+    pensize = ["big", "average", "micro"]
+
+    def __init__(race, height, skincolor, haircolor, pensize)
+    
+    
