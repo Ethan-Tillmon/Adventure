c047ab9892ee014d7a6047d1c831c3112d231407
Ethan-Tillmon <ethantillmon@amdg.rockhursths.edu>
2020-03-10 08:48:30 -0500

    Game Intro

--- Intro.py
+++ Intro.py
@@ -4,6 +4,10 @@ def intro():
     name = input("Who are you? ")
     welcome(name)
 
+def class():
+    global name
+    print("Hey " + name + ", welcome to the family")
+
 def welcome(name):
     print("Welcome, " + name)
 
