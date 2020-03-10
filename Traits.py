6c95a9564a2c348b3d67d036518b85116ac5cc26
Ethan-Tillmon <ethantillmon@amdg.rockhursths.edu>
2020-03-10 08:47:55 -0500

    Orphan Traits

--- Traits.py
+++ Traits.py
@@ -5,6 +5,25 @@ class traits(objects):
     haircolor = ["brunette", "blond", "dirty blond", "red"]
     pensize = ["big", "average", "micro"]
 
-    def __init__(race, height, skincolor, haircolor, pensize)
+    def __init__(self,name,race,height,skincolor,haircolor,pensize):
+        self.name = name
+        self.race = race
+        self.height = height
+        self.skincolor = skincolor
+        self.haircolor = haircolor
+        self.pensize = pensize
+
+    def chooseRace(self):
+        self.race = input("What race is your character? Pick your number."
+            "Black: 1"
+            "Caucasion: 2"
+            "Hispanic: 3"
+            "Asian: 4"
+            "Middle eastern: 5"
+            "Your race number: ")
+
+    def __str__(self):
+        if(race == 1):Black
+            
     
     
